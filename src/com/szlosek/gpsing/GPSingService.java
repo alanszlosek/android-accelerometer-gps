0a977dc00f24f70a35015863edbaa6486edea249
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2012-07-13 20:54:13 -0700

    indents

--- src/com/szlosek/gpsing/GPSingService.java
+++ src/com/szlosek/gpsing/GPSingService.java
@@ -203,11 +203,7 @@ public class GPSingService extends Service implements SensorEventListener, Locat
 
 		// Determine whether to discard the location or not ...
 		// hope this ends up being a quick calculation
-		if (currentBestLocation != null) {
-			if (isBetterLocation(location, currentBestLocation)){
-				currentBestLocation = location;
-			}
-		} else {
+		if (isBetterLocation(location, currentBestLocation)){
 			currentBestLocation = location;
 		}
 	
@@ -251,99 +247,94 @@ public class GPSingService extends Service implements SensorEventListener, Locat
 	  * @param currentBestLocation  The current Location fix, to which you want to compare the new one
 	  */
 	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
-	    if (currentBestLocation == null) {
-		// A new location is always better than no location
-		return true;
-	    }
+		if (currentBestLocation == null) {
+			// A new location is always better than no location
+			return true;
+		}
 
-	    // Check whether the new location fix is newer or older
-	    long timeDelta = location.getTime() - currentBestLocation.getTime();
-	    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-	    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-	    boolean isNewer = timeDelta > 0;
+		// Check whether the new location fix is newer or older
+		long timeDelta = location.getTime() - currentBestLocation.getTime();
+		boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+		boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+		boolean isNewer = timeDelta > 0;
 
-	    // If it's been more than two minutes since the current location, use the new location
-	    // because the user has likely moved
-	    if (isSignificantlyNewer) {
-		return true;
-	    // If the new location is more than two minutes older, it must be worse
-	    } else if (isSignificantlyOlder) {
+		// If it's been more than two minutes since the current location, use the new location
+		// because the user has likely moved
+		if (isSignificantlyNewer) {
+			return true;
+		// If the new location is more than two minutes older, it must be worse
+		} else if (isSignificantlyOlder) {
+			return false;
+		}
+
+		// Check whether the new location fix is more or less accurate
+		int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+		boolean isLessAccurate = accuracyDelta > 0;
+		boolean isMoreAccurate = accuracyDelta < 0;
+		boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+		// Check if the old and new location are from the same provider
+		boolean isFromSameProvider = isSameProvider(location.getProvider(), currentBestLocation.getProvider());
+
+		// Determine location quality using a combination of timeliness and accuracy
+		if (isMoreAccurate) {
+			return true;
+		} else if (isNewer && !isLessAccurate) {
+			return true;
+		} else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+			return true;
+		}
 		return false;
-	    }
-
-	    // Check whether the new location fix is more or less accurate
-	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
-	    boolean isLessAccurate = accuracyDelta > 0;
-	    boolean isMoreAccurate = accuracyDelta < 0;
-	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
-
-	    // Check if the old and new location are from the same provider
-	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
-		    currentBestLocation.getProvider());
-
-	    // Determine location quality using a combination of timeliness and accuracy
-	    if (isMoreAccurate) {
-		return true;
-	    } else if (isNewer && !isLessAccurate) {
-		return true;
-	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-		return true;
-	    }
-	    return false;
 	}
 
 	/** Checks whether two providers are the same */
 	private boolean isSameProvider(String provider1, String provider2) {
-	    if (provider1 == null) {
-	      return provider2 == null;
-	    }
-	    return provider1.equals(provider2);
+		if (provider1 == null) {
+			return provider2 == null;
+		}
+		return provider1.equals(provider2);
 	}
 
 
 	// OTHER
  	
  	public class LocationsOpenHelper extends SQLiteOpenHelper {
- 	    public LocationsOpenHelper(Context context) {
- 	        super(context, "locations.db", null, 1);
- 	    }
+		public LocationsOpenHelper(Context context) {
+			super(context, "locations.db", null, 1);
+		}
 
- 	    @Override
- 	    public void onCreate(SQLiteDatabase db) {
- 	        db.execSQL("create table locations (milliseconds integer, latitude real, longitude real, altitude real, gpsStart integer, accuracy real, best integer);");
- 	    }
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL("create table locations (milliseconds integer, latitude real, longitude real, altitude real, gpsStart integer, accuracy real, best integer);");
+		}
 
- 		@Override
- 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
- 			// TODO Auto-generated method stub
- 			
- 		}
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+		}
  	}
  	
  	
  	public void sleep(int w) {
-	    AlarmManager mgr = (AlarmManager)getSystemService(ALARM_SERVICE);
-        Intent i = new Intent(this, GPSingReceiver.class);
-        Calendar cal = new GregorianCalendar();
-        
-        if (w == 0) {
-	        if (iSinceMotion < 3) {
-	        	Log.d("GPSing", "Waiting 30 seconds");
-	        	cal.add(Calendar.SECOND, 5);
-	        } else {
-	        	
-	    		Log.d("GPSing", "Waiting 60 seconds");
-	    		cal.add(Calendar.SECOND, 10);
-	        }
-        } else {
-        	cal.add(Calendar.SECOND, w);
-        }
+		AlarmManager mgr = (AlarmManager)getSystemService(ALARM_SERVICE);
+		Intent i = new Intent(this, GPSingReceiver.class);
+		Calendar cal = new GregorianCalendar();
 
-        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
-        mgr.set(
-        		AlarmManager.RTC_WAKEUP,
-        		cal.getTimeInMillis(),
-        		pi);
+		if (w == 0) {
+			if (iSinceMotion < 3) {
+				Log.d("GPSing", "Waiting 30 seconds");
+				cal.add(Calendar.SECOND, 5);
+			} else {
+
+				Log.d("GPSing", "Waiting 60 seconds");
+				cal.add(Calendar.SECOND, 10);
+			}
+		} else {
+			cal.add(Calendar.SECOND, w);
+		}
+
+		PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+		mgr.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
 	}
 	
 	@Override
@@ -389,7 +380,7 @@ public class GPSingService extends Service implements SensorEventListener, Locat
 	@Override
 	public IBinder onBind(Intent arg0) {
 		Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
-        //return mMessenger.getBinder();
+		//return mMessenger.getBinder();
 		return null;
 	}
 
