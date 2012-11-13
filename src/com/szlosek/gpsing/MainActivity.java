1aa114cd6c0e751d107149acae7bbf168a1b4b10
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2012-11-12 17:42:15 -0800

    no title

--- src/com/szlosek/gpsing/MainActivity.java
+++ src/com/szlosek/gpsing/MainActivity.java
@@ -170,6 +170,7 @@ public class MainActivity extends MapActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		
 		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
